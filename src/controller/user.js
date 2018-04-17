59b7711f486b9b1c0fa9901cd50052ac3abb354f
Victor Leung <victor.tw.leung@hk.ey.com>
2018-04-17 11:15:41 +0800

    Change custom scheme

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,8 +2,8 @@ import { getUsers } from '../service/user';
 
 export const get = (ctx) => {
 	ctx.status = 302;
-	ctx.set('Location', 'intent://open?link_click_id=123456#Intent;scheme=com.hsbc.digial.peak.paymebiz://oauth/redirect;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;' )
-	ctx.redirect = ('intent://open?link_click_id=123456#Intent;scheme=com.hsbc.digial.peak.paymebiz://oauth/redirect;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;')
+	ctx.set('Location', 'intent://open?link_click_id=123456#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;' )
+	ctx.redirect = ('intent://open?link_click_id=123456#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;')
     ctx.body = 'Redirecting to new intent'
 };
 
