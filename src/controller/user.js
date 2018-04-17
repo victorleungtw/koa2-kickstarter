ab1b126f638381f440538c2664ca286ddb110901
Victor Leung <victor.tw.leung@hk.ey.com>
2018-04-17 13:50:25 +0800

    Change url

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,8 +2,8 @@ import { getUsers } from '../service/user';
 
 export const get = (ctx) => {
 	ctx.status = 307;
-	ctx.set('Location', 'intent://scan/#Intent;scheme=zxing;package=com.google.zxing.client.android;end' )
-	ctx.redirect = ('intent://scan/#Intent;scheme=zxing;package=com.google.zxing.client.android;end')
+	ctx.set('Location', 'intent://scan/#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;end' )
+	ctx.redirect = ('intent://scan/#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;end')
     ctx.body = 'Redirecting to new intent'
 };
 
