18185cb4c05b46cd8524f0408a4b30abe3f1df06
Victor Leung <victor.tw.leung@hk.ey.com>
2018-04-17 14:47:00 +0800

    Change from 307 to 302

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,7 @@
 import { getUsers } from '../service/user';
 
 export const get = (ctx) => {
-	ctx.status = 307;
+	ctx.status = 302;
 	ctx.set('Location', 'intent://home?action=email-verification&hash=8ae335a572f783f0fd6146596c4f6cf1&salt=3e8bc8ccfac6058abe10b22d5b957278&status=success/#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;end' )
 	ctx.redirect = ('intent://home?action=email-verification&hash=8ae335a572f783f0fd6146596c4f6cf1&salt=3e8bc8ccfac6058abe10b22d5b957278&status=success/#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;end')
     ctx.body = 'Redirecting to new intent'
