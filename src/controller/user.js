ee85b8b4480551e710a377f58484ebecc4245604
Victor Leung <victor.tw.leung@hk.ey.com>
2018-04-17 11:05:33 +0800

    Add location

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,6 +2,7 @@ import { getUsers } from '../service/user';
 
 export const get = (ctx) => {
 	ctx.status = 301;
+	ctx.set('Location', 'intent://open?link_click_id=123456#Intent;scheme=com.hsbc.digial.peak.paymebiz://oauth/redirect;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;' )
 	ctx.redirect = ('intent://open?link_click_id=123456#Intent;scheme=com.hsbc.digial.peak.paymebiz://oauth/redirect;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;')
     ctx.body = 'Redirecting to new intent'
 };
