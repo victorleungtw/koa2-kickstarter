07def028938aac6f976e46cdd94443860a503383
Victor Leung <victor.tw.leung@hk.ey.com>
2018-04-17 11:02:28 +0800

    Update endpoint

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,9 @@
 import { getUsers } from '../service/user';
 
 export const get = (ctx) => {
-  ctx.body = { users: getUsers(), time: Date.now() };
+	ctx.status = 301;
+	ctx.redirect = ('intent://open?link_click_id=123456#Intent;scheme=com.hsbc.digial.peak.paymebiz://oauth/redirect;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;')
+    ctx.body = 'Redirecting to new intent'
 };
 
 export const post = (ctx) => {
