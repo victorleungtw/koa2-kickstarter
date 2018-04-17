aafeef21ad102f4fe62a9c8236c89a9e80863f46
Victor Leung <victor.tw.leung@hk.ey.com>
2018-04-17 13:48:41 +0800

    Try intent

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,8 +2,8 @@ import { getUsers } from '../service/user';
 
 export const get = (ctx) => {
 	ctx.status = 307;
-	ctx.set('Location', 'intent://open?link_click_id=123456#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;' )
-	ctx.redirect = ('intent://open?link_click_id=123456#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;')
+	ctx.set('Location', 'intent://scan/#Intent;scheme=zxing;package=com.google.zxing.client.android;end' )
+	ctx.redirect = ('intent://scan/#Intent;scheme=zxing;package=com.google.zxing.client.android;end')
     ctx.body = 'Redirecting to new intent'
 };
 
