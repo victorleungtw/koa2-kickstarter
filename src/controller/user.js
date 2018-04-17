16a95c8474066ad8094bf1d70c584daabf2d8667
Victor Leung <victor.tw.leung@hk.ey.com>
2018-04-17 11:21:00 +0800

    Change redirect url

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,8 +2,8 @@ import { getUsers } from '../service/user';
 
 export const get = (ctx) => {
 	ctx.status = 302;
-	ctx.set('Location', 'intent://open?link_click_id=123456#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;' )
-	ctx.redirect = ('intent://open?link_click_id=123456#Intent;scheme=paymebiz;package=com.hsbc.digital.peak.PaymeBiz.dev.debug;')
+	ctx.set('Location', 'http://com.hsbc.digial.peak.paymebiz/user' )
+	ctx.redirect = ('http://com.hsbc.digial.peak.paymebiz/user')
     ctx.body = 'Redirecting to new intent'
 };
 
